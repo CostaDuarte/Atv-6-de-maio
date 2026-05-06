8b39890ea0b413e70d28a42edf02c0d26f23cd0e
CostaDuarte <nicolas.d.costa6@edu.senai.br>
2026-05-06 06:59:35 -0300

    :sparkles: feat: adicionado codigo para o usuário ver a contagem alterando em tempo real ao invez de mostrar tudo de uma vez

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //*1) Criar um programa em JS que mostra uma contagem regressiva de 10 até 1 (escolher o looping de repetição mais apropriado - lembre-se você já sabe quanto vai contar).
 let message = "";
-for (i = 10; i >= 1; i--) {
+for (i = 10; i >= 2; i--) {
     message = `${message} ${i}`;
     alert(message)
 }
-alert(message + ", fim da contagem regressiva!")
+alert(message + " 1, fim da contagem regressiva!")
