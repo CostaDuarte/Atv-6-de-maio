b761750f4213e621124392029d4322898490247c
CostaDuarte <nicolas.d.costa6@edu.senai.br>
2026-05-06 10:06:21 -0300

    :bug: fix: arrumado problema caso usúario não insira número

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-//* 3) Criar um programa em JS que peça um número ao usuário e mostre a tabuada dele de 1 até 10.
+//todo 3) Criar um programa em JS que peça um número ao usuário e mostre a tabuada dele de 1 até 10.
+
 let numUm;
 let message = "";
-numUm = Number(
-  prompt("Por favor insira um número e eu mostrarei a tabuada dele de 1 a 10"),
-);
+do
+  numUm = Number(
+    prompt(
+      "Por favor insira um número e eu mostrarei a tabuada dele de 1 a 10",
+    ),
+  );
+while (isNaN(numUm));
 for (let i = 1; i < 11; i++) {
   message = `${message}${i} x ${numUm} = ${i * numUm}
 `;
