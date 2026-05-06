9436151284738352ce844dbb44a35c2001d6c3ac
CostaDuarte <nicolas.d.costa6@edu.senai.br>
2026-05-06 11:22:04 -0300

    :bulb: docs: comentado o que a variável montante significa

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //* 2) Criar um programa em JS que calcule a soma de todos os números de 1 até 10.
 //* 💡 Dica: armazene a soma em uma variável.
 //! ⚠️ Obrigatório somar a cada iteração e não apenas somar tudo de uma vez.
+
+//? que faz a variável montante?
+//* montante é a variavél da soma de todos os números
 let montante = 0;
 for (let i = 1; i <= 10; i++) {
     montante = montante + i;
