8aae0ab28ac54c3e7e948fa3c6af4570d875aee9
CostaDuarte <nicolas.d.costa6@edu.senai.br>
2026-05-08 06:53:14 -0300

    :broom: cleanup: formatado o código para melhor clean code

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-//*1) Criar um programa em JS que mostra uma contagem regressiva de 10 até 1 (escolher o looping de repetição mais apropriado - lembre-se você já sabe quanto vai contar).
+//todo 1) Criar um programa em JS que mostra uma contagem regressiva de 10 até 1 (escolher o looping de repetição mais apropriado - lembre-se você já sabe quanto vai contar).
+
 let message = "";
 for (i = 10; i >= 2; i--) {
-    message = `${message} ${i}`;
-    alert(message)
+  message = `${message} ${i}`;
+  alert(message);
 }
-alert(message + " 1, fim da contagem regressiva!")
+alert(message + " 1, fim da contagem regressiva!");
