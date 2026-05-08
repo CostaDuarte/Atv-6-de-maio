ca2147699b36645c5a6e28d4fbb2b1441fa8bece
CostaDuarte <nicolas.d.costa6@edu.senai.br>
2026-05-08 06:53:45 -0300

    :broom: cleanup: formatado o código para melhor clean code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//* 2) Criar um programa em JS que calcule a soma de todos os números de 1 até 10.
+//todo 2) Criar um programa em JS que calcule a soma de todos os números de 1 até 10.
 //* 💡 Dica: armazene a soma em uma variável.
 //! ⚠️ Obrigatório somar a cada iteração e não apenas somar tudo de uma vez.
 
@@ -6,7 +6,7 @@
 //* montante é a variavél da soma de todos os números
 let montante = 0;
 for (let i = 1; i <= 10; i++) {
-    montante = montante + i;
-    alert(montante)
+  montante = montante + i;
+  alert(montante);
 }
-alert(`o resultado de todos os números de 1 a 10 somados é: ${montante}`)
+alert(`o resultado de todos os números de 1 a 10 somados é: ${montante}`);
